06cfa2a73d2dcee665032471d5b39f420cb80025
Eri Ishikawa <eri-ishikawa@proton.me>
2024-07-23 14:49:17 -0700

    finished demos

--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
@@ -4,6 +4,19 @@ public class Calculations
 {
     public static void main(String[] args)
     {
+        // Calculate how many donuts per student
+        int numberOfStudents = 4;
+        int numberofDonuts = 22;
 
+        int donutsPerStudent = numberofDonuts / numberOfStudents;
+        int remainingDonuts = numberofDonuts % numberOfStudents;
+        double donutsPerStudentDouble = (double)numberofDonuts / numberOfStudents;
+
+        System.out.println("Number of donuts per student");
+        System.out.println("Total donuts: " + numberofDonuts);
+        System.out.println("Total students: " + numberOfStudents);
+        System.out.println("Donuts per student: " + donutsPerStudent);
+        System.out.println("Remaining Donuts: " + remainingDonuts);
+        System.out.println("Donuts per student (double): " + donutsPerStudentDouble);
     }
 }
