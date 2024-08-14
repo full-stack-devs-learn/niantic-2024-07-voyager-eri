c2d70d338b39903401c3a2aa76efdc6d69508230
Eri (Windows) <eri-ishikawa@proton.me>
2024-08-14 00:49:33 -0700

    ★ DAY 17 ★ Finished Categories and Products Controllers

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/CategoriesController.java
@@ -1,10 +1,35 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Category;
 import com.niantic.services.CategoryDao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class CategoriesController
 {
     CategoryDao categoryDao = new CategoryDao();
+
+    @GetMapping("/categories")
+    public String getAllCategories(Model model)
+    {
+        ArrayList<Category> categories = categoryDao.getCategories();
+
+        StringBuilder builder = new StringBuilder();
+
+        model.addAttribute("categories", categories);
+        return "categories/index";
+    }
+
+    @GetMapping("/categories/{id}")
+    public String details(Model model, @PathVariable int id)
+    {
+        var category = categoryDao.getCategoryById(id);
+        model.addAttribute("category", category);
+        return "categories/details";
+    }
 }
--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
@@ -1,5 +1,36 @@
 package com.niantic.controllers;
 
+import com.niantic.models.Product;
+import com.niantic.services.ProductDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
 public class ProductsController
 {
+    ProductDao productsDao = new ProductDao();
+
+    @GetMapping("/products")
+    public String getProductsByCategory(Model model, Integer categoryId)
+    {
+        ArrayList<Product> products;
+
+        if(categoryId == null)
+        {
+            products = productsDao.getProductsByCategory(0);
+        }
+        else
+        {
+            products = productsDao.getProductsByCategory(categoryId);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        model.addAttribute("products", products);
+        return "products/index";
+
+    }
 }
