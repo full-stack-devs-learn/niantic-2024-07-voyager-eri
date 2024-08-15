a14da2423d91820c11f7a78a9006f1d69481b8cb
Eri (Windows) <eri-ishikawa@proton.me>
2024-08-14 19:52:39 -0700

    ★ DAY 18 ★ Worked on Product Controller

--- sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
+++ sprint-1/17-thymeleaf-part-1/exercise_mvc-web-application/src/main/java/com/niantic/controllers/ProductsController.java
@@ -7,7 +7,9 @@ import com.niantic.services.ProductDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 
@@ -38,6 +40,7 @@ public class ProductsController
 
         model.addAttribute("products", products);
         model.addAttribute("category", category);
+        model.addAttribute("headTitle", "Products in " + category.getCategoryName() + " | Northwind Traders");
 
         return "products/index";
     }
@@ -52,5 +55,66 @@ public class ProductsController
         model.addAttribute("category", category);
 
         return "products/details";
+    }
+
+    @GetMapping("/products/add")
+    public String addProduct(Model model)
+    {
+        model.addAttribute("product", new Product());
+        model.addAttribute("action", "add");
+        model.addAttribute("headTitle", "Add New Product | Northwind Traders");
+
+        return "products/add_edit";
+    }
+
+    @PostMapping("products/add")
+    public String addProduct(Model model, @ModelAttribute("product") Product product)
+    {
+        productsDao.addProduct(product);
+        model.addAttribute("product", product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("products/{id}/edit")
+    public String editProduct(Model model, @PathVariable int id)
+    {
+        Product product = productsDao.getProduct(id);
+
+        model.addAttribute("product", product);
+        model.addAttribute("headTitle", product.getProductName() + " | Edit Product | Northwind Traders");
+
+        return "products/add_edit";
+    }
+
+    @PostMapping("products/{id}/edit")
+    public String editProduct(@ModelAttribute("product") Product product, @PathVariable int id)
+    {
+        product.setProductId(id);
+        productsDao.updateProduct(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("products/{id}/delete")
+    public String deleteProduct(Model model, @PathVariable int id)
+    {
+        Product product = productsDao.getProduct(id);
+
+        if(product == null)
+        {
+            model.addAttribute("message", String.format("There is no product with id %d", id));
+            return "404";
+        }
+
+        model.addAttribute("product", product);
+        model.addAttribute("headTitle", product.getProductName() + " | Delete Category | Northwind Traders");
+        return "products/delete";
+    }
+
+    @PostMapping("products/{id}/delete")
+    public String deleteCategory(@PathVariable int id)
+    {
+        productsDao.deleteProduct(id);
+        return "redirect:/products";
     }
 }
