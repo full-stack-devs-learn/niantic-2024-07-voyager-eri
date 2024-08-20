b224e35ee4f1054eaef0db1c4eff75517ceb62fe
Eri (Windows) <eri-ishikawa@proton.me>
2024-08-20 13:11:18 -0700

    ★ S2-D2 ★ Finished initial Wizard and Knight classes

--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Knight.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Knight.java
@@ -1,5 +1,42 @@
 package com.niantic;
 
-public class Knight
+public class Knight extends Character
 {
+    private int armor;
+
+    public Knight(String name, int health, int level, int experience, int armor)
+    {
+        super(name, health, level, experience);
+        this.armor = armor;
+    }
+
+    public int getArmor()
+    {
+        return armor;
+    }
+
+    @Override
+    public void takeDamage(int damage)
+    {
+        int reducedDamage;
+
+        if(damage > armor)
+        {
+            reducedDamage = damage - armor;
+            takeDamage(reducedDamage);
+        }
+    }
+
+    @Override
+    public void levelUp()
+    {
+        super.levelUp();
+        if(!isDefeated()) armor += 5;
+    }
+
+    @Override
+    public String specialAbility()
+    {
+        return "Armor Shield";
+    }
 }
--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
@@ -1,5 +1,55 @@
 package com.niantic;
 
-public class Wizard
+public class Wizard extends Character
 {
+    private int mana;
+
+    public Wizard(String name, int health, int level, int experience, int mana)
+    {
+        super(name, health, level, experience);
+        this.mana = mana;
+    }
+
+    public int getMana()
+    {
+        return mana;
+    }
+
+    public void castSpell(Character target)
+    {
+        if (isDefeated())
+        {
+            System.out.println(this.name + " has been defeated and cannot attack another player.");
+            return;
+        }
+
+        System.out.println(this.name + " attacks " + target.getName());
+
+        if(mana >= 10)
+        {
+            target.takeDamage(attackDamage * 2);
+            mana -= 10;
+        }
+    }
+
+    public void regenerateMana(int amount)
+    {
+        if(!isDefeated())
+        {
+            mana += amount;
+        }
+    }
+
+    @Override
+    public void levelUp()
+    {
+        super.levelUp();
+        if(!isDefeated()) regenerateMana(10);
+    }
+
+    @Override
+    public String specialAbility()
+    {
+        return "Cast Spells";
+    }
 }
