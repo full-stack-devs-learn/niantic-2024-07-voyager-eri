71db884d0b6b150a20aa6228cc5f1845b9c4bfc7
Eri (Windows) <eri-ishikawa@proton.me>
2024-08-24 16:16:42 -0700

    ★ S2-D4 ★ Deleted class Action

--- sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/application/CardGameApplication.java
+++ sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/application/CardGameApplication.java
@@ -32,12 +32,13 @@ public class CardGameApplication
 
     private void dealCards()
     {
+        int numOfStartingCards = 7;
         deck.shuffle();
         System.out.println();
         System.out.println("Deck has been shuffled!");
 
         // each player starts out with 7 cards
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < numOfStartingCards; i++)
         {
             for(Player player : players)
             {
@@ -72,7 +73,7 @@ public class CardGameApplication
 
             UserInterface.displayPlayerTurn(player.getName());
 
-            Action.discardCard(player, deck, discardPile);
+            playerTurn(player);
 
             // As soon as one player has 0 cards in their hand, we end the game
             if(player.getHand().getCardCount() == 0)
@@ -85,5 +86,91 @@ public class CardGameApplication
                 queuedPlayers.offer(player);
             }
         }
+    }
+
+    // This is the main action in the game
+    public void playerTurn(Player player)
+    {
+        Card topCard = discardPile.getTopCard();
+        UserInterface.displayTopCardInDiscardPile(topCard);
+
+        // Get a list of all the cards that can be put in the discard pile
+        List<Card> discardableCards = player.getHand().getCards().stream()
+                .filter(card -> card.getColor().equals(topCard.getColor())
+                        || card.getNumber() == topCard.getNumber())
+                .toList();
+
+        // If there are no cards that can be put in the discard pile...
+        if(discardableCards.isEmpty())
+        {
+            drawCard(player, topCard);
+        }
+        else
+        {
+            discardCard(player, discardableCards);
+        }
+    }
+
+    public void drawCard(Player player, Card topCard)
+    {
+        // Check to see if the deck to draw cards from is empty; if it is, refill the deck
+        if(deck.getCardCount() == 0)
+        {
+            System.out.println("The draw deck is empty! Refilling...");
+            deck.refillDeck(discardPile);
+        }
+
+        // Player draws a card from the draw deck
+        Card card = deck.takeCard();
+        System.out.println(player.getName() + " drew a card because they didn't have any cards to play.");
+
+        // Can the card be immediately played?
+        if(card.getColor().equals(topCard.getColor()) || card.getNumber() == topCard.getNumber())
+        {
+            if(player.isUser())
+            {
+                String choice = UserInterface.displayOptionToPlayDrawnCard(card);
+                if (choice.equals("y"))
+                {
+                    discardPile.addCard(card);
+                    System.out.println("You played the card that you drew.");
+                }
+                else if(choice.equals("n"))
+                {
+                    System.out.println("You chose not to play the card.");
+                }
+            }
+            else
+            {
+                discardPile.addCard(card);
+                System.out.println(player.getName() + " played the card that they drew.");
+            }
+        }
+        else
+        {
+            player.getHand().dealTo(card);
+        }
+    }
+
+    public void discardCard(Player player, List<Card> discardableCards)
+    {
+        // Player puts one of their playable cards in the discard pile
+
+        Card cardToDiscard;
+
+        if(player.isUser())
+        {
+            UserInterface.displayUserCards(player.getHand().getCards());
+            UserInterface.displayUserPlayableCards(discardableCards);
+            cardToDiscard = UserInterface.selectUserPlayableCard(discardableCards);
+        }
+        else
+        {
+            cardToDiscard = discardableCards.getFirst();
+        }
+
+        player.getHand().getCards().remove(cardToDiscard);
+        discardPile.addCard(cardToDiscard);
+        UserInterface.displayCardToPlay(player.getName(), cardToDiscard);
     }
 }
