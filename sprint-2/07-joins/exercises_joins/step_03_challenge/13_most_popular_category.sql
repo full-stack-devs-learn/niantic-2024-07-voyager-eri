69e0c850aad1ac628ae362769a47a0ba9902d54c
Eri (Windows) <eri-ishikawa@proton.me>
2024-08-27 16:05:59 -0700

    ★ S2-D7 ★ Fixed 13

--- sprint-2/07-joins/exercises_joins/step_03_challenge/13_most_popular_category.sql
+++ sprint-2/07-joins/exercises_joins/step_03_challenge/13_most_popular_category.sql
@@ -10,12 +10,14 @@
 USE sakila;
 
 SELECT c.name
-	, COUNT(i.film_id) AS number_of_times_rented
+	, COUNT(i.inventory_id) AS number_of_times_rented
 FROM category AS c
 INNER JOIN film_category AS fc
 	ON c.category_id = fc.category_id
 INNER JOIN inventory AS i
 	ON fc.film_id = i.film_id
+INNER JOIN rental AS r
+	on i.inventory_id = r.inventory_id
 GROUP BY c.name
 ORDER BY number_of_times_rented DESC
 ;
