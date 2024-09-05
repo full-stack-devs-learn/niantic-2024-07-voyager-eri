8bafe5f6d50910aa7b5574e62021a40442e06c7b
Eri (Windows) <eri-ishikawa@proton.me>
2024-09-04 19:40:49 -0700

    ★ S2-D12★ Added products api controller

---
+++ sprint-2/12_javascript-and-thymeleaf/exercise_javascript-and-thymeleaf/src/main/java/com/niantic/controllers/apis/ProductsApiController.java
@@ -0,0 +1,21 @@
+package com.niantic.controllers.apis;
+
+import com.niantic.models.Product;
+import com.niantic.services.ProductDao;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class ProductsApiController
+{
+    private ProductDao productDao = new ProductDao();
+
+    @GetMapping("/api/products/category/{catId")
+    public ArrayList<Product> productsByCategory(@PathVariable int catId)
+    {
+        return productDao.getProductsByCategory(catId);
+    }
+}
