fb1515d04cf3cbbedc48dadd9c895b8969c8bf8e
Eri (Windows) <eri-ishikawa@proton.me>
2024-09-11 14:31:23 -0700

    ★ S3-D1 ★ Finished Sword and test cases

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
@@ -2,11 +2,43 @@ package com.niantic;
 
 public class Sword extends Weapon
 {
+    public Sword(String name, int damage)
+    {
+        super(name, damage);
+    }
+
    @Override
    public int attack()
    {
-       return 0;
+       if(percentCharged <= 90)
+       {
+           percentCharged += 10;
+       }
+       return getDamage();
    }
 
+   @Override
+    public int powerAttack()
+   {
+       if(percentCharged < 50)
+       {
+           return getDamage();
+       }
+       else if(percentCharged <= 90)
+       {
+           percentCharged -= 50;
+           return getDamage() * 2;
+       }
+       else
+       {
+           percentCharged = 0;
+           return getDamage() * 4;
+       }
+   }
 
+   @Override
+    public int getRange()
+   {
+       return 1;
+   }
 }
---
+++ sprint-3/01-abstraction/exercise_abstraction/src/test/java/com/niantic/SwordTest.java
@@ -0,0 +1,135 @@
+package com.niantic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwordTest
+{
+    private Sword sword;
+    private String name;
+    private int damage = 10;
+
+    @BeforeEach
+    public void setup()
+    {
+        sword = new Sword(name, damage);
+    }
+
+    @Test
+    public void attack_shouldDealDefaultDamageToOtherCharacter()
+    {
+        // arrange
+        int expectedDamage = 10;
+
+        //act
+        int actualDamage = sword.attack();
+
+        //assert
+        assertEquals(expectedDamage, actualDamage, "Because the Sword's set damage is 10.");
+    }
+
+    @Test
+    public void attack_shouldIncreasePercentCharged_By10()
+    {
+        // arrange
+        int expectedCharge = 20;
+
+        // act
+        sword.attack();
+        sword.attack();
+
+        int actualCharge = sword.getPercentCharged();
+
+        // assert
+        assertEquals(expectedCharge, actualCharge, "Because weapon attacked 2 times, should be 20%.");
+    }
+
+    @Test
+    public void attack_shouldNotIncreasePercentCharged_Beyond100()
+    {
+        // arrange
+        int expectedCharge = 100;
+
+        // act
+        sword.setPercentCharged(expectedCharge);
+        sword.attack();
+
+        int actualCharge = sword.getPercentCharged();
+
+        // assert
+        assertEquals(expectedCharge, actualCharge, "Because percentCharge should not increase beyond 100%");
+    }
+
+    @Test
+    public void powerAttack_shouldPerformRegularAttack_ifPercentChargeUnder50()
+    {
+        // arrange
+        int expectedDamage = 10;
+
+        // act
+        int actualDamage = sword.powerAttack();
+
+        // assert
+        assertEquals(expectedDamage, actualDamage, "percentCharge was 0, so powerAttack should perform regular attack with damage 10");
+    }
+
+    @Test
+    public void powerAttack_shouldDeliverDoubleDamage_ifPercentCharge50AboveAnd90Under()
+    {
+        // arrange
+        int expectedDamage = 20;
+        sword.setPercentCharged(50);
+
+        // act
+        int actualDamage = sword.powerAttack();
+
+        // assert
+        assertEquals(expectedDamage, actualDamage, "percentCharge was 50, so powerAttack should deal 20 damage");
+    }
+
+    @Test
+    public void powerAttack_shouldReducePercentChargeBy50_whenPercentCharge50Above()
+    {
+        // arrange
+        int expectedPercent = 0;
+        sword.setPercentCharged(50);
+
+        // act
+        sword.powerAttack();
+        int actualPercent = sword.getPercentCharged();
+
+        // assert
+        assertEquals(expectedPercent, actualPercent, "percentCharge before powerAttack: 50 - percentCharge after attack should be 0");
+    }
+
+    @Test
+    public void powerAttack_shouldDeliverQuadrupleDamage_ifPercentChargeIs100()
+    {
+        // arrange
+        int expectedDamage = 40;
+        sword.setPercentCharged(100);
+
+        // act
+        int actualDamage = sword.powerAttack();
+
+        // assert
+        assertEquals(expectedDamage, actualDamage, "Because percentCharged was 100, so it should have performed 4x damage attack");
+    }
+
+    @Test
+    public void powerAttack_shouldReducePercentChargeToZero_whenPercentCharge100()
+    {
+        // arrange
+        int expectedCharge = 0;
+        sword.setPercentCharged(100);
+
+        // act
+        sword.powerAttack();
+        int actualCharge = sword.getPercentCharged();
+
+        // assert
+        assertEquals(expectedCharge, actualCharge, "Because using powerAttack at 100% charge should reduce percentCharge to 0");
+    }
+}
