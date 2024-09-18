d4485f21dd1e1b36b1a01b5cdb3c12f27e62c20f
Eri (Windows) <eri-ishikawa@proton.me>
2024-09-18 14:00:09 -0700

    ★ S3-D6 ★ Finished 2: Get products by category

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/services/MySqlProductDao.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/services/MySqlProductDao.java
@@ -1,4 +1,65 @@
 package com.niantic.services;
 
-public class MySqlProductDao {
+import com.niantic.models.Product;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySqlProductDao implements ProductDao
+{
+    private final JdbcTemplate jdbcTemplate;
+
+    public MySqlProductDao()
+    {
+        String databaseUrl = "jdbc:mysql://localhost:3306/northwind";
+        String userName = "root";
+        String password = "P@ssw0rd";
+        DataSource dataSource = new BasicDataSource(){{
+            setUrl(databaseUrl);
+            setUsername(userName);
+            setPassword(password);
+        }};
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(int catId)
+    {
+        List<Product> products = new ArrayList<>();
+
+        String sql = """
+                    SELECT product_id
+                        , category_id
+                        , product_name
+                        , quantity_per_unit
+                        , unit_price
+                        , units_in_stock
+                        , units_on_order
+                        , reorder_level
+                    FROM products
+                    WHERE category_id = ?
+                    """;
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql, catId);
+
+        while(row.next())
+        {
+            int productId = row.getInt("product_id");
+            String productName = row.getString("product_name");
+            String quantityPerUnit = row.getString("quantity_per_unit");
+            double unitPrice = row.getDouble("unit_price");
+            int unitsInStock = row.getInt("units_in_stock");
+            int unitsOnOrder = row.getInt("units_on_order");
+            int reorderLevel = row.getInt("reorder_level");
+
+            Product product = new Product(productId, catId, productName, quantityPerUnit, unitPrice, unitsInStock, unitsOnOrder, reorderLevel);
+            products.add(product);
+        }
+        return products;
+    }
 }
